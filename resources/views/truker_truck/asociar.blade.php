63d6bd7917d0a88b9694ce0d781d4553fa4687f0
lf-ander <ander.anp@gmail.com>
2024-06-10 21:04:17 -0500

    first commit

---
+++ resources/views/truker_truck/asociar.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+<form action="{{ route('trucker_truck.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <br>
+                    <h3>Camionero</h3>
+                    <select class="container-sm col-6" name="trucker_id">   {{-- importante --}}  
+                        @foreach ($truckers as $cust)
+                            <option  value="{{ $cust->id }}">{{ $cust->id }} - {{ $cust->nombre}}</option>
+                        @endforeach
+                    </select>
+                    <br>
+                    <br>
+                    <br>
+                    <h3>camion:</h3>
+                    <select class="container-sm col-6" name="truck_id">
+                        @foreach ($trucks as $prod)
+                            <option  value="{{ $prod->id }}">{{ $prod->id }} - {{ $prod->tipo }}</option>
+                        @endforeach
+                    </select>
+                    <br> <br>
+                    <button class="btn btn-primary" type="submit">Enviar Formulario:</button>
+                </form>
+    </center>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('paquete/listar', [PackageController::class, 'listar'])->name('package.listar');
+Route::get('paquete/create', [PackageController::class, 'create'])->name('package.create');
+Route::post('paquete/store', [PackageController::class, 'store'])->name('package.store');
+Route::get('paquete/{paq}', [PackageController::class, 'show'])->name('package.show');
+Route::put('paquete/{paq}', [PackageController::class, 'update'])->name('package.update');
+Route::delete('paquete/{paq}', [PackageController::class, 'destroy'])->name('package.destroy');
+Route::get('paquete/{paq}/edit', [PackageController::class, 'edit'])->name('package.edit');
+
+
+Route::get('trucker/truck/listar',[TruckerTruckController::class, 'listar'])->name('trucker_truck.listar');
+Route::get('trucker/asociar/truck',[TruckerTruckController::class, 'asociar'])->name('trucker_truck.asociar');
+Route::post('trucker/store',[TruckerTruckController::class, 'store'])->name('trucker_truck.store');
+
+
